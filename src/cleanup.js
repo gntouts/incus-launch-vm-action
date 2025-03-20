62c1ab9ec84da8706d3754ffc917a91d7e3c2c6b
Georgios Ntoutsos <gntouts@nubificus.co.uk>
2025-03-20 14:06:27 +0000

    Add JS placeholder files
    Signed-off-by: Georgios Ntoutsos <gntouts@nubificus.co.uk>

---
+++ src/cleanup.js
@@ -0,0 +1,10 @@
+const os = require('os');
+const { execSync } = require('child_process');
+const { getInput, setFailed, setOutput } = require('@actions/core');
+const fs = require('fs');
+
+try {
+    console.log("Starting cleanup");
+} catch (error) {
+    setFailed(error.message);
+}
---
+++ src/launch.js
@@ -0,0 +1,10 @@
+const os = require('os');
+const { execSync } = require('child_process');
+const { getInput, setFailed, setOutput } = require('@actions/core');
+const fs = require('fs');
+
+try {
+    console.log("Starting VM launch");
+} catch (error) {
+    setFailed(error.message);
+}
