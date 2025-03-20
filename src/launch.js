0b69aca64bb7d1873f36379832eb5aed293a488a
Georgios Ntoutsos <gntouts@nubificus.co.uk>
2025-03-20 15:11:40 +0000

    Add Action functionality
    Signed-off-by: Georgios Ntoutsos <gntouts@nubificus.co.uk>

--- src/launch.js
+++ src/launch.js
@@ -12,52 +12,52 @@ try {
     const diskSize = getInput('disk_size');
     const incusProfile = getInput('incus_profile');
     const incusProject = getInput('incus_project');
+    const vmName = getInput('vm_name');
+    const vmDescription = getInput('vm_description');
 
     console.log(`incus_remote: ${incusRemote}`);
     console.log(`incus_image: ${incusImage}`);
     console.log(`cpu_cores: ${cpuCores}`);
     console.log(`memory: ${memory}`);
     console.log(`disk_size: ${diskSize}`);
-    console.log(`incus_profile: ${incusProfile}`);  
-    console.log(`incus_project: ${incusProject}`);
+    console.log(`incus_profile: ${incusProfile}`);
+    console.log(`vm_name: ${vmName}`);
+    console.log(`vm_description: ${vmDescription}`);
 
+    // spawn new VM
+    var spawnCmd = `sudo incus launch `;
+    spawnCmd += `${incusRemote}:${incusImage} `;
+    spawnCmd += `${incusProject}:${vmName} `;
+    spawnCmd += `--project ${incusProject} `;
+    spawnCmd += `--profile ${incusProfile} `;
+    spawnCmd += `--description "${vmDescription}" `;
+    spawnCmd += `-c limits.cpu=${cpuCores} `;
+    spawnCmd += `-c limits.memory=${memory}GiB `;
+    spawnCmd += `-d root,size=${diskSize}GiB `;
+    spawnCmd += `--vm`;
+    console.log(`spawnCmd: ${spawnCmd}`);
 
+    execSync(spawnCmd, { stdio: 'inherit' });
 
-//     incus_remote:
-//     description: 'Name of the Incus remote'
-//     required: true
-//   incus_image:
-//     description: 'Name of the Incus image'
-//     required: false
-//     default: 'images:ubuntu/22.04/cloud'
-//   cpu_cores:
-//     description: 'Number of CPU cores'
-//     required: false
-//     default: '1'
-//   memory:
-//     description: 'Amount of memory in GiB'
-//     required: false
-//     default: '2'
-//   disk_size:
-//     description: 'Size of the disk in GiB'
-//     required: false
-//     default: '10'
-//   incus_profile:
-//     description: 'Name of the Incus profile'
-//     required: false
-//     default: 'default'
-//   incus_project:
-//     description: 'Name of the Incus project'
-//     required: false
-//     default: 'default'
-//   cleanup:
-//     description: 'Whether to cleanup the VM after the action'
-//     required: false
-//     default: 'false'
-//   snapshot:
-//     description: 'Whether to create a snapshot of the VM'
-//     required: false
-//     default: 'false'
+    // get VM IP
+    var getIpCmd = `sudo incus info ${incusRemote}:${vmName} --project ${incusProject} `;
+    getIpCmd += `| awk '/inet: / {print $2}' | grep -v  "127.0.0.1" | cut -d'/' -f1"`;
+    console.log(`getIpCmd: ${getIpCmd}`);
+    var ip = execSync(getIpCmd, { stdio: 'pipe' }).toString().trim();
+    const retries = 30;
+    var retry = 0;
+    while (ip === "") {
+        console.log("Waiting for VM to be ready...");
+        ip = execSync(getIpCmd, { stdio: 'pipe' }).toString().trim();
+        retry++;
+        if (retry === retries) {
+            throw new Error("Failed to get IP address");
+        }
+        sleep(4000);
+    }
+    // set IP as output
+    setOutput('incus_vm_ip', ip);
+    console.log(`VM IP: ${ip}`);
 } catch (error) {
     setFailed(error.message);
 }
