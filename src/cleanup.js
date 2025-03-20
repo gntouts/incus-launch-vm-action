720fe3ea1647b12f80ffa3edc16b2ae6f766e54f
Georgios Ntoutsos <gntouts@nubificus.co.uk>
2025-03-20 16:03:00 +0000

    minor fix
    Signed-off-by: Georgios Ntoutsos <gntouts@nubificus.co.uk>

--- src/cleanup.js
+++ src/cleanup.js
@@ -15,9 +15,10 @@ try {
     console.log(`snapshot: ${snapshot}`);
 
     console.log("Stopping VM");
-    var stopCmd = `sudo incus stop ${incusRemote}:${vmName} --project ${incusProject}`;
+    var stopCmd = `sudo incus stop ${incusRemote}:${vmName} --force --project ${incusProject}`;
     console.log(`stopCmd: ${stopCmd}`);
     execSync(stopCmd, { stdio: 'inherit' });
+    console.log("VM stopped");
 
     if (snapshot === "true") {
         console.log("Snapshotting VM");
@@ -25,6 +26,7 @@ try {
         console.log(`snapshotCmd: ${snapshotCmd}`);
         execSync(snapshotCmd, { stdio: 'inherit' });
         setOutput('snapshot_name', `${vmName}-snapshot`);
+        console.log("Snapshot created");
     }
 
     if (cleanUp === "true") {
@@ -32,6 +34,7 @@ try {
         var cleanUpCmd = `sudo incus delete ${incusRemote}:${vmName} --force --project ${incusProject}`;
         console.log(`cleanUpCmd: ${cleanUpCmd}`);
         execSync(cleanUpCmd, { stdio: 'inherit' });
+        console.log("VM deleted");
     }
 
 } catch (error) {
